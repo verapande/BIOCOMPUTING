99cdda9ddc4f89647c84ceaf94f7ebbe0bf17803
Vera Pande--vpande@wm.edu--vpande---Geoffrey Zahn [gzahn@wm.edu] <vpande@bora.sciclone.wm.edu>
2025-10-20 20:33:25 -0400

    Add correct version of second script

--- assignments/assignment_6/scripts/02_flye_2.9.6_conda_install.sh
+++ assignments/assignment_6/scripts/02_flye_2.9.6_conda_install.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-# installs Flye v2.9.6 into a user env (no shared lock issues)
-
-mkdir -p $HOME/.conda/pkgs $HOME/.conda/envs
-export CONDA_PKGS_DIRS=$HOME/.conda/pkgs
-export CONDA_ENVS_PATH=$HOME/.conda/envs
+# flye_2.9.6_conda_install.sh
 
 module load miniforge3
 source /sciclone/apps/miniforge3-24.9.2-0/etc/profile.d/conda.sh
 
-# try 2.9.6, else 2.9.5 (bioconda has 2.9.5 reliably)
-mamba create -y -n flye-env -c bioconda -c conda-forge flye=2.9.6 \
-  || mamba create -y -n flye-env -c bioconda -c conda-forge flye=2.9.5
-
+mamba create -y -n flye-env -c bioconda -c conda-forge flye=2.9.6
 conda activate flye-env
 flye -v
 conda env export --no-builds > flye-env.yml
