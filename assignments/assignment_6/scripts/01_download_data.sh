c5f18d65a0a3db233f07b52464eb8aeed8ad6ed5
Vera Pande--vpande@wm.edu--vpande---Geoffrey Zahn [gzahn@wm.edu] <vpande@bora.sciclone.wm.edu>
2025-10-20 20:26:27 -0400

    Add correct version of first script

--- assignments/assignment_6/scripts/01_download_data.sh
+++ assignments/assignment_6/scripts/01_download_data.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Downloads ONT dataset for E. coli phages into ./data directory
+# Downloads the dataset for the E. coli phage into ./data directory
 
 mkdir -p data
 cd data
 
 wget https://ftp.sra.ebi.ac.uk/vol1/fastq/SRR120/032/SRR12012232/SRR12012232_1.fastq.gz
 
-echo "downloaded to: $(pwd)"
+echo "Download complete."
+
