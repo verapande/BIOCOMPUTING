808a0b02589d6b0128e0a03600a7ffaffe143c9e
Vera Pande--vpande@wm.edu--vpande---Geoffrey Zahn [gzahn@wm.edu] <vpande@bora.sciclone.wm.edu>
2025-10-20 20:44:12 -0400

    -

--- assignments/assignment_6/scripts/03_run_flye_local.sh
+++ assignments/assignment_6/scripts/03_run_flye_local.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
-# run Flye using local build
+# 03_run_flye_local.sh
 
-# add both common build paths
-export PATH="$PATH:$HOME/BIOCOMPUTING/programs/Flye/bin:$HOME/programs/Flye/bin"
+# add local Flye build to my PATH
+export PATH="$PATH:$HOME/BIOCOMPUTING/programs/Flye/bin"
 
 mkdir -p assemblies/assembly_local
 rm -rf assemblies/assembly_local/*
 
-# high-quality ONT
-flye --nano-hq data/*.fastq* \
-     --out-dir assemblies/assembly_local \
-     --threads 6 \
-     --genome-size 170k 2>&1 | tee assemblies/assembly_local/local_flye.log
+flye --nano-hq data/*.fastq* --out-dir assemblies/assembly_local --threads 6 --genome-size 170k
 
-# don't crash if file missing
-mv assemblies/assembly_local/assembly.fasta assemblies/assembly_local/local_assembly.fasta 2>/dev/null || \
-echo "no assembly.fasta produced; see assemblies/assembly_local/local_flye.log"
-
-# keep only fasta + log
-find assemblies/assembly_local -type f ! -name 'local_assembly.fasta' ! -name 'local_flye.log' -delete 2>/dev/null
+# rename output files
+mv assemblies/assembly_local/assembly.fasta assemblies/assembly_local/local_assembly.fasta
+mv assemblies/assembly_local/flye.log assemblies/assembly_local/local_flye.log
