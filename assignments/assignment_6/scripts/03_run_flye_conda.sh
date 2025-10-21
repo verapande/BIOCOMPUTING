013bb7bd981308eccc303ad8990cb7b7414a25b0
Vera Pande--vpande@wm.edu--vpande---Geoffrey Zahn [gzahn@wm.edu] <vpande@bora.sciclone.wm.edu>
2025-10-20 20:40:13 -0400

    Add correct version of third script

--- assignments/assignment_6/scripts/03_run_flye_conda.sh
+++ assignments/assignment_6/scripts/03_run_flye_conda.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# run Flye using the conda environment "flye-env"
-
-# conda lives in your home so it finds your env
-export CONDA_PKGS_DIRS=$HOME/.conda/pkgs
-export CONDA_ENVS_PATH=$HOME/.conda/envs
+# 02_run_flye_conda.sh
 
 module load miniforge3
 source /sciclone/apps/miniforge3-24.9.2-0/etc/profile.d/conda.sh
@@ -12,17 +8,9 @@ conda activate flye-env
 mkdir -p assemblies/assembly_conda
 rm -rf assemblies/assembly_conda/*
 
-# NOTE: high-quality ONT reads -> --nano-hq (not --nano-raw)
-flye --nano-hq data/*.fastq* \
-     --out-dir assemblies/assembly_conda \
-     --threads 6 \
-     --genome-size 170k 2>&1 | tee assemblies/assembly_conda/conda_flye.log
+flye --nano-hq data/*.fastq* --out-dir assemblies/assembly_conda --threads 6 --genome-size 170k
 
-# rename if present; otherwise just print a hint (no if/fi used)
-mv assemblies/assembly_conda/assembly.fasta assemblies/assembly_conda/conda_assembly.fasta 2>/dev/null || \
-echo "no assembly.fasta produced; see assemblies/assembly_conda/conda_flye.log"
-
-# keep only fasta + log
-find assemblies/assembly_conda -type f ! -name 'conda_assembly.fasta' ! -name 'conda_flye.log' -delete 2>/dev/null
+mv assemblies/assembly_conda/assembly.fasta assemblies/assembly_conda/conda_assembly.fasta
+mv assemblies/assembly_conda/flye.log assemblies/assembly_conda/conda_flye.log
 
 conda deactivate
