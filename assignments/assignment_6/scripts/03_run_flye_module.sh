0e86d84893bd4acfc43c731c3f7c40f9d2a205cd
Vera Pande--vpande@wm.edu--vpande---Geoffrey Zahn [gzahn@wm.edu] <vpande@bora.sciclone.wm.edu>
2025-10-20 20:48:39 -0400

    -

--- assignments/assignment_6/scripts/03_run_flye_module.sh
+++ assignments/assignment_6/scripts/03_run_flye_module.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# run Flye using the HPC module found at: Flye/gcc-11.4.1/2.9.6
+# 04_run_flye_module.sh
 
 module load Flye/gcc-11.4.1/2.9.6
 flye -v
@@ -7,12 +7,7 @@ flye -v
 mkdir -p assemblies/assembly_module
 rm -rf assemblies/assembly_module/*
 
-flye --nano-hq data/*.fastq* \
-     --out-dir assemblies/assembly_module \
-     --threads 6 \
-     --genome-size 170k 2>&1 | tee assemblies/assembly_module/module_flye.log
+flye --nano-hq data/*.fastq* --out-dir assemblies/assembly_module --threads 6 --genome-size 170k
 
-mv assemblies/assembly_module/assembly.fasta assemblies/assembly_module/module_assembly.fasta 2>/dev/null || \
-echo "no assembly.fasta produced; see assemblies/assembly_module/module_flye.log"
-
-find assemblies/assembly_module -type f ! -name 'module_assembly.fasta' ! -name 'module_flye.log' -delete 2>/dev/null
+mv assemblies/assembly_module/assembly.fasta assemblies/assembly_module/module_assembly.fasta
+mv assemblies/assembly_module/flye.log assemblies/assembly_module/module_flye.log
